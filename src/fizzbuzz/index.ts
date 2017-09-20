871f228f7bdc6a5074fc5b8b205b03b84ab252e1
kutyel <flaviocorpa@gmail.com>
2017-09-20 10:32:43 +0200

    Fix unit tests and prettier 💅🏼

--- src/fizzbuzz/index.ts
+++ src/fizzbuzz/index.ts
@@ -1,13 +1,13 @@
-export const isFizz = (x: number) => x !== 0 && x % 3 === 0
-export const isBuzz = (x: number) => x !== 0 && x % 5 === 0
+export const isFizz = (x: number) => x % 3 === 0
+export const isBuzz = (x: number) => x % 5 === 0
 
 export default (length = 100) => {
   let result = []
   for (let index = 1; index <= length; index++) {
     result.push(
       isFizz(index) && isBuzz(index)
-        ? "FizzBuzz"
-        : isFizz(index) ? "Fizz" : isBuzz(index) ? "Buzz" : index
+        ? 'FizzBuzz'
+        : isFizz(index) ? 'Fizz' : isBuzz(index) ? 'Buzz' : index
     )
   }
   return result
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -1,22 +1,26 @@
-import fizzbuzz, { isFizz, isBuzz } from "../src/fizzbuzz"
+import fizzbuzz, { isFizz, isBuzz } from '../src/fizzbuzz'
 
-describe("FizzBuzz", () => {
-  const result = fizzbuzz()
+describe('FizzBuzz', () => {
+  const aux = fizzbuzz()
 
-  it("isFizz() should check correcty multiples of 3", () =>
+  it('isFizz() should check correcty multiples of 3', () =>
     expect([3, 6, 9, 12, 15].every(isFizz)).toBeTruthy())
 
-  it("isBuzz() should check correcty multiples of 5", () =>
-    expect([5, 10, 15, 20].every(isBuzz)).toBeTruthy())
+  it('isBuzz() should check correcty multiples of 5', () =>
+    expect([5, 10, 15, 20, 25].every(isBuzz)).toBeTruthy())
 
   it('should return "Fizz" for multiples of 3', () =>
-    result.forEach((x, index) => isFizz(index) && expect(x).toBe("Fizz")))
+    aux.forEach(
+      (x, i) => isFizz(i + 1) && !isBuzz(i + 1) && expect(x).toBe('Fizz')
+    ))
 
   it('should return "Buzz" for multiples of 5', () =>
-    result.forEach((x, index) => isBuzz(index) && expect(x).toBe("Buzz")))
+    aux.forEach(
+      (x, i) => isBuzz(i + 1) && !isFizz(i + 1) && expect(x).toBe('Buzz')
+    ))
 
   it('should return "FizzBuzz" for multiples of 3 and 5', () =>
-    result.forEach(
-      (x, index) => isFizz(index) && isBuzz(index) && expect(x).toBe("FizzBuzz")
+    aux.forEach(
+      (x, i) => isFizz(i + 1) && isBuzz(i + 1) && expect(x).toBe('FizzBuzz')
     ))
 })
