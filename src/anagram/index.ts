853cf19886081357b3b4e5e47a71c70323e6fa12
kutyel <flaviocorpa@gmail.com>
2017-09-24 00:04:43 +0200

    Refactor out `Set` from anagram.ts ⚡

--- src/anagram/index.ts
+++ src/anagram/index.ts
@@ -2,15 +2,13 @@
  * A word is an anagram if you can rearrange its characters to produce the second word
  */
 export default (s1: string, s2: string): boolean => {
-  const chars = s1
-    .split('')
-    .reduce(
-      (map, char) => map.set(char, (map.get(char) || 0) + 1),
-      new Map<string, number>()
-    )
-  for (const char of s2.split('')) {
-    if (!chars.has(char)) return false
-    chars.set(char, (chars.get(char) || 0) - 1)
+  const chars: any = {}
+  for (const c of s1.split('')) {
+    chars[c] = (chars[c] || 0) + 1
   }
-  return Array.from(chars.values()).every(v => v === 0)
+  for (const c of s2.split('')) {
+    if (!chars[c]) return false
+    chars[c] = (chars[c] || 0) - 1
+  }
+  return Object.values(chars).every(v => v === 0)
 }
