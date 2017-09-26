499e9e206bcaec5eaa113f2a12086d5316c0c2a5
kutyel <flaviocorpa@gmail.com>
2017-09-26 08:37:25 +0200

    Add bubbleSort to index 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import areAnagrams from './anagram'
+import bubbleSort from './bubbleSort'
 import deepClone from './deepClone'
 import fizzBuzz from './fizzbuzz'
 import isPalindrome from './palindrome'
 
 export default {
   areAnagrams,
+  bubbleSort,
   deepClone,
   fizzBuzz,
   isPalindrome
