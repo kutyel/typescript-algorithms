7236d82499fe35075d013b14e264cd0b425f0caf
Chad Sheets <cjsheets@gmail.com>
2017-10-22 15:11:32 -0800

    Prefer to not clobber duplicates

--- src/quickSort/index.ts
+++ src/quickSort/index.ts
@@ -10,7 +10,7 @@ export default function quickSort<T>(list: T[]): T[] {
   if (list.length < 2) return list
 
   const pivot = random(list)
-  const less = list.filter(i => i < pivot)
+  const less = list.filter(i => i <= pivot)
   const greater = list.filter(i => i > pivot)
 
   return [...quickSort(less), pivot, ...quickSort(greater)]
